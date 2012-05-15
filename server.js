e070937172d45dc0119927f1cc84cb089fb3eb7f
hhuai <8863824@gmail.com>
2012-05-15 17:38:02 +0800

    change spwan to exec

--- server.js
+++ server.js
@@ -3,29 +3,27 @@ var child_process = require('child_process');
 var url = require('url');
 var querystring = require('querystring');
 
-function exe_cmd (response, cmd, args) {
+function exe_cmd (response, cmd) {
   console.log(cmd);
-  console.log(args);
-  var ls = child_process.spawn(cmd, args.split(' '));
   var out_data = '';
-  ls.stdout.on('data', function (data) {
-    out_data += data;
+  var ls = child_process.exec(cmd, function(error,stdout,stderr) {
+      response.writeHead(200, {'Content-Type': 'text/plain'});
+      response.end(out_data);
+
+      if (error !== null) {
+        console.log('exec error: ' + error);
+      }
   });
-  ls.stdout.on('end', function(){
-    console.log(out_data);
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end(out_data);
-    out_data = '';
-  })
 }
 
 var http = require('http');
 http.createServer(function (req, res) {
-  url_info = url.parse(req.url);
+  var url_info = url.parse(req.url);
   if (url_info.pathname == '/exe_cmd') {
-    params = querystring.parse(req.url);
-    console.log(params);
-    exe_cmd(res, params['/exe_cmd?cmd'], params.args);  
+    var params = querystring.unescape(req.url)
+    var index_string = 'exe_cmd?cmd=';
+    var cmd = params.slice(params.indexOf(index_string)+index_string.length);
+    exe_cmd(res, cmd);  
   } else {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello World\n');
