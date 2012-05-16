27d7c775a4b2f69938c55bcb8786659129767299
hhuai <8863824@gmail.com>
2012-05-16 12:55:45 +0800

    输出优化

--- run_remote_ruby_test.py
+++ run_remote_ruby_test.py
@@ -2,61 +2,65 @@ import sublime, sublime_plugin
 import urllib  
 import urllib2  
 import threading  
-
-event1 = threading.Event()
+import functools
 
 class RemoteRunApiCall(threading.Thread):  
-    def __init__(self,view, timeout):    
+    def __init__(self,finish_callback, timeout):    
+        super(RemoteRunApiCall,self).__init__()
         self.timeout = timeout  
         self.result = None  
-        
-        self.view = view
-        threading.Thread.__init__(self)  
+        self.finish_callback = finish_callback
   
     def run(self):  
         try:  
-            data = urllib.urlencode({'cmd': 'rspec', 'args':'spec'})  
+            data = urllib.urlencode({'cmd': 'rspec', 'args':'spec --drb'})  
             request = urllib2.Request('http://localhost:8080/exe_cmd?'+data,  
                 headers={"User-Agent": "Sublime Prefixr"})  
             
             http_file = urllib2.urlopen(request, timeout=self.timeout)  
             self.result = http_file.read()  
-            event1.clear()
-            return  
-  
         except (urllib2.HTTPError) as (e):  
-            err = '%s: HTTP error %s contacting API' % (__name__, str(e.code))  
+            self.result = '%s: HTTP error %s contacting API' % (__name__, str(e.code))  
         except (urllib2.URLError) as (e):  
-            err = '%s: URL error %s contacting API' % (__name__, str(e.reason))  
-  
-        sublime.error_message(err)  
-        self.result = False  
+            self.result = '%s: URL error %s contacting API' % (__name__, str(e.reason)) 
 
-    
+        sublime.set_timeout(functools.partial(self.finish_callback, self.result+"\n"), 1) 
 
 output_view = None
 class RemoteRunCommand(sublime_plugin.TextCommand):
+  thread = None
   def run(self, edit):
-    thread = RemoteRunApiCall(self.view, 5)
-    thread.start()
-    thread.join()
-    self.output(thread.result)
-  
-  def show_output_view(self):
-      global output_view
-      if not output_view:
-          output_view = self.view.window().get_output_panel("evalsel")
-      self.view.window().run_command('show_panel', {'panel': 'output.evalsel'})
-
-  def output(self, info):
+    self.show_tests_panel()
+    self.thread = RemoteRunApiCall(self.append_data, 5)
+    self.thread.start()
+    
+  def show_tests_panel(self):
     global output_view
-    self.show_output_view()
+    if output_view is None:
+      output_view = sublime.active_window().get_output_panel("tests")
+    self.clear_test_view()
+    sublime.active_window().run_command("show_panel", {"panel": "output.tests"})
+
+  def clear_test_view(self):
+    global output_view
     output_view.set_read_only(False)
     edit = output_view.begin_edit()
-     
-    output_view.insert(edit, output_view.size(), info)
-    self.scroll_to_view_end()
- 
+    output_view.erase(edit, sublime.Region(0, output_view.size()))
     output_view.end_edit(edit)
     output_view.set_read_only(True)
-    self.show_output_view()
+
+  def append_data(self, data):
+    global output_view
+    str = data.decode("utf-8")
+    str = str.replace('\r\n', '\n').replace('\r', '\n')
+
+    selection_was_at_end = (len(output_view.sel()) == 1
+      and output_view.sel()[0]
+        == sublime.Region(output_view.size()))
+    output_view.set_read_only(False)
+    edit = output_view.begin_edit()
+    output_view.insert(edit, output_view.size(), str)
+    if selection_was_at_end:
+      output_view.show(output_view.size())
+    output_view.end_edit(edit)
+    output_view.set_read_only(True)
