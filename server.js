06d8acab8cc6cd8f52ea8fa14eb1c9de21765e15
hhuai <8863824@gmail.com>
2012-05-24 13:42:38 +0800

    加入启动和停止rails server功能

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
 var net =require('net');
 var child_process = require('child_process');
+var rails_ps = null;
 
 var server = net.createServer(function(socket){
   console.log('server connected.');
   socket.on('end', function(){
     console.log('server disconnected.')
   });
-  var ls ;
+  var ls = null;
+  
   socket.on('data',function(data){
     console.log(data.toString());
     var params = JSON.parse(data);
-    ls = child_process.spawn(params.cmd,params.args,{cwd:params.cwd});
-    ls.stdout.on('data', console_stdout);
-    ls.stderr.on('data', console_stderr);
-    ls.on('exit',console_exit);
+    ls = process(params, socket);
+    if (ls) {
+      ls.stdout.on('data', console_stdout);
+      ls.stderr.on('data', console_stderr);
+      ls.on('exit',console_exit);
+    };
   });
 
   function console_stdout(data) {
@@ -38,4 +42,42 @@ var server = net.createServer(function(socket){
 });
 
 server.listen(8000);
+
+function process (params, socket) {
+  var cmd = params.cmd
+  if (cmd=='?') {socket.end(help()); return null;};
+  if (cmd=='starts' || cmd=='stops' || cmd=='restarts') {return start_rails_cmd(params,socket)};
+  return child_process.spawn(params.cmd,params.args,{cwd:params.cwd});
+}
+
+function start_rails_cmd (params,socket) {
+    var ls = null;
+    if (params.cmd=='starts') {
+      if (rails_ps) {
+        socket.end('rails server already started.\n');  
+        return null;
+      };
+      args = params.args;
+      args.unshift('server');
+      ls = child_process.spawn('rails',args,{cwd:params.cwd});
+      rails_ps = ls;
+    } else if(params.cmd == 'stops'){
+      if (rails_ps) {
+        rails_ps.kill('SIGINT'); rails_ps=null;
+        socket.end('rails server stoped.\n');  
+      } else {
+        socket.end('rails server not started.\n');  
+      }
+      ls = null;
+    } else if(params.cmd == 'restarts'){
+      socket.end('unsupport.\n');  
+    }
+    return ls;
+  }
+
+function help () {
+  return 'starts - start the rails web server.\n' +
+         'stops - stop the rails web server.\n' +
+         'restarts - restart the rails web server.\n' ;
+}
 	
